e0f4351db9b45a2202e118a621a4c022bee5e35f
NguyenDangKhoaDepTrai <khoa.nguyen2211621@hcmut.eud.vn>
2025-04-10 22:35:56 +0700

    add env in porject

--- src/server.js
+++ src/server.js
@@ -1,26 +1,51 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from './config/mongodb'
+import { env } from './config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. Hello ${env.AUTHOR}, BE server is running successfully at hostname: ${ env.APP_HOST } and port: ${ env.APP_PORT }/`)
+  })
+
+  // Khi server BE tắt thì sẽ gọi hàm này
+  // HIỆN VẪN CHƯA DÙNG ĐƯỢC
+  exitHook(() => {
+    console.log('BE server is shutting down...')
+    CLOSE_DB()
+    console.log('Closed MongoDB connection!')
+  })
+}
+
+// Chỉ khi kết nối tới Database thành công thì mới start server BE lên
+// Immediately Invoked Function Expression (IIFE) để gọi hàm CONNECT_DB
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlas!')
+
+    START_SERVER()
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error)
+    process.exit(0)
+  }
+})()
+
+// Chỉ khi kết nối tới Database thành công thì mới start server BE lên
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB Cloud Atlas!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error('Error connecting to MongoDB:', error)
+//     process.exit(0)
+//   })
