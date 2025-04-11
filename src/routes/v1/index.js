348bbb17236bde54e8dc9eef26ca49a3381308ef
NguyenDangKhoaDepTrai <khoa.nguyen2211621@hcmut.eud.vn>
2025-04-11 10:18:35 +0700

    feat: express_router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,21 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes.js'
+
+const Router = express.Router()
+
+// Check APIs v1/status
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    message: 'APIs V1 are ready to use!',
+    status: StatusCodes.OK
+  })
+})
+
+// Board APIs
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
