29f08b9c724e9a9bdd4ff11b837d769cc10e2c9b
NguyenDangKhoaDepTrai <khoa.nguyen2211621@hcmut.eud.vn>
2025-04-12 20:13:12 +0700

    create_model

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,5 @@
-import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formater'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -11,12 +11,18 @@ const createNew = async (reqBody) => {
     }
 
     // Gọi tới Model để lưu dữ liệu bản ghi newBoard vào Database
+    const createdBoard = await boardModel.createNew(newBoard)
+    // console.log('createdBoard: ', createdBoard)
+
+    // Lấy bản ghi Board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    // console.log('getNewBoard: ', getNewBoard)
 
     // Làm thêm các xử lý logic khác với các Colection khác tùy vào đặc thù dự án
     // Bắn email, notification, ...về cho admin khi có 1 cái board mới được tạo, ...
 
     // Trả về dữ liệu cho Controller, luôn dữ liệu về cho Controller
-    return newBoard
+    return getNewBoard
   } catch (error) { throw error }
 }
 
