64023d46e3d05a397b5fb72be1e97b91eb04be30
NguyenDangKhoaDepTrai <khoa.nguyen2211621@hcmut.eud.vn>
2025-04-15 00:20:09 +0700

    update WHITELIST_DOMAINS again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   // Không cần localhost nữa vì ở file cors.js đã luôn luôn cho phép môi trường dev
-  'https://trello-web-omega-weld.vercel.app/'
+  'https://trello-web-omega-weld.vercel.app'
 ]
 
 export const BOARD_TYPES = {
